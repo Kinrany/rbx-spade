bb846ec06c6434db2e4d987a5d774395780510a1
Ruslan Fadeev <kinrany@yandex.com>
2020-12-20 21:38:48 +0300

    Extract slateRegionSize

--- src/server/main.server.ts
+++ src/server/main.server.ts
@@ -11,5 +11,9 @@ const spawnPlatformRegion = new Region3(
 workspace.Terrain.FillRegion(spawnPlatformRegion, 4, Enum.Material.WoodPlanks);
 
 // create the slate wall
-const slateRegion = new Region3(new Vector3(-1000, -1000, -1000), new Vector3(-spawnPlatformSize, 1000, 1000));
+const slateRegionSize = 1000;
+const slateRegion = new Region3(
+	new Vector3(-slateRegionSize, -slateRegionSize, -slateRegionSize),
+	new Vector3(-spawnPlatformSize, slateRegionSize, slateRegionSize),
+);
 workspace.Terrain.FillRegion(slateRegion, 4, Enum.Material.Slate);
