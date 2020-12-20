8d3fc78d6448163f202fd3eff2eb465a42ed85fe
Ruslan Fadeev <kinrany@yandex.com>
2020-12-20 21:39:38 +0300

    Delete client debug output

--- src/client/main.client.ts
+++ src/client/main.client.ts
@@ -2,8 +2,3 @@ import { makeHello, makeWorld } from "shared/module";
 
 print(makeHello("main.client.ts"));
 print(makeWorld("greeting"));
-
-game.GetService("RunService").Heartbeat.Connect(() => {
-	const player = game.GetService("Players").LocalPlayer;
-	// print(player.Character?.GetPrimaryPartCFrame().Position);
-});
