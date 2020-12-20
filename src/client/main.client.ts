e77d1838549ff53306c6f202181af15d583330f2
Ruslan Fadeev <kinrany@yandex.com>
2020-12-20 19:55:37 +0300

    Add the wall

--- src/client/main.client.ts
+++ src/client/main.client.ts
@@ -2,3 +2,8 @@ import { makeHello, makeWorld } from "shared/module";
 
 print(makeHello("main.client.ts"));
 print(makeWorld("greeting"));
+
+game.GetService("RunService").Heartbeat.Connect(() => {
+	const player = game.GetService("Players").LocalPlayer;
+	// print(player.Character?.GetPrimaryPartCFrame().Position);
+});
--- src/server/main.server.ts
+++ src/server/main.server.ts
@@ -1,4 +1,15 @@
-import { makeHello, makeWorld } from "shared/module";
+export {};
 
-print(makeHello("main.server.ts"));
-print(makeWorld("hello"));
+const workspace = game.GetService("Workspace");
+
+// create the player spawn
+const spawnPlatformSize = 25;
+const spawnPlatformRegion = new Region3(
+	new Vector3(-spawnPlatformSize, -5, -spawnPlatformSize),
+	new Vector3(spawnPlatformSize, 0, spawnPlatformSize),
+);
+workspace.Terrain.FillRegion(spawnPlatformRegion, 4, Enum.Material.WoodPlanks);
+
+// create the slate wall
+const slateRegion = new Region3(new Vector3(-1000, -1000, -1000), new Vector3(-spawnPlatformSize, 1000, 1000));
+workspace.Terrain.FillRegion(slateRegion, 4, Enum.Material.Slate);
